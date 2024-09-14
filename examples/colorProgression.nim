1914e4294bb8d2464f0637b712f9135c379b688c
Cardeal Russo <cardealrusso@vaguiner.eu.org>
2024-09-14 14:59:41 -0300

    Update colorProgression.nim

--- examples/colorProgression.nim
+++ examples/colorProgression.nim
@@ -3,13 +3,10 @@ import festim
 var app = init(Fenster, "color progression", 800, 600)
 var x, y, color = 0
 
-proc nextColor(): uint32 =
-  result = uint32(color)
-  color += 1
-
 while app.loop():
-  app[x, y] = nextColor()
+  app[x, y] = uint32(color)
 
+  color += 1
   x += 1
   if x >= app.width:
     x = 0
