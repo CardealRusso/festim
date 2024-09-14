8848b10a8d02e99d7321925f702b6cff44420a78
Cardeal Russo <cardealrusso@vaguiner.eu.org>
2024-09-14 16:15:29 -0300

    Update red_square_exit_on_esc_key.nim

--- examples/red_square_exit_on_esc_key.nim
+++ examples/red_square_exit_on_esc_key.nim
@@ -2,9 +2,6 @@ import festim
 
 var app = init(Fenster, "red square. exit on esc key", 800, 600)
 
-# check keys codes here https://theasciicode.com.ar/
-# pressed key returns 1
-
 while app.loop() and app.keys[27] == 0:
   for x in 350 .. 450:
     for y in 250 .. 350:
