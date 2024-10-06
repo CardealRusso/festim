ae7d5e6a8c37d600e28f3537e20f4fd2b10c32f2
Cardeal Russo <cardealrusso@vaguiner.eu.org>
2024-10-06 08:09:18 -0300

    Update white_noise.nim

--- examples/white_noise.nim
+++ examples/white_noise.nim
@@ -5,5 +5,4 @@ var app = init(Fenster, "White noise with fenstim", 800, 600)
 while app.loop and app.keys[27] == 0:
   for x in 0 ..< app.width:
     for y in 0 ..< app.height:
-      #app[x, y] = rand(0xFFFFFF) or 0xFF000000
-      app[x, y] = rand(int32.high)
+      app[x, y] = rand(uint32)
